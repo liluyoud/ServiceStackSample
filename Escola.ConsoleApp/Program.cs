dd8bdf98216d8d6e0ddc5eec83f1ae2ed73e5cef
Liluyoud Cury de Lacerda <liluyoud@outlook.com>
2021-11-23 22:08:55 -0400

    console app

---
+++ Escola.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Escola.Core.Commands;
+using ServiceStack;
+
+var client = new JsonServiceClient("https://localhost:7278");
+
+var response = await client.GetAsync(new QueryCitiesCommand());
+
+var cities = response.Results;
+
+foreach (var city in cities)
+{
+    Console.WriteLine(city.Name);
+}
+
+Console.ReadKey();
